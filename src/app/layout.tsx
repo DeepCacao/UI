271bb3be69d420cc4a556fe1b83b93dff20d1e95
rubiuscoffee <bsquintero_usistemas@intep.edu.co>
2025-11-24 23:38:56 -0500

    feat: Add smooth scrolling functionality by integrating the new `SmoothScroll` component and `lenis` dependency.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "./landing/components/nav";
 import Footer from "./landing/components/footer";
+import SmoothScroll from "../components/smooth-scroll";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SmoothScroll />
         <Nav />
         {children}
         <Footer />
